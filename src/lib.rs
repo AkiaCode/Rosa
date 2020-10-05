a310c6aa26bd867f93bdd37c16a326dede346578
Catry <catry.me@gmail.com>
2020-10-06 08:56:16 +0900

    get_file lint

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,9 @@ use std::io::{Read, copy};
 use std::fs::File;
 
 pub fn get_file(url: &str) -> impl Read {
-	let mut request = ureq::get(url);
-
-	let response = request.call();
-
-	response.into_reader()
+	let request: ureq::Response = ureq::get(url).call();
+	
+	request.into_reader()
 }
 
 pub fn download<F: ?Sized>(mut file: &mut F, path: &str, name: &str) where F: Read {
