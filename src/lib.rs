3a62eb341d8ba6620c1e8698314b2497e2af5e13
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-10-06 13:37:37 -0400

    add json feature, handle errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use ureq;
+use std::result::Result;
 use std::io::{Read, copy};
 use std::fs::File;
 
-pub fn get_file(url: &str) -> impl Read {
-	let mut request = ureq::get(url);
+pub fn get_file(url: &str) -> Result<impl Read, &ureq::Error> {
+	let response = ureq::get(url).call();
+	if let Some(err) = response.synthetic_error() {
+		return Err(err)
+	}
 
-	let response = request.call();
-
-	response.into_reader()
+	Ok(response.into_reader())
 }
 
 pub fn download<F: ?Sized>(mut file: &mut F, path: &str, name: &str) where F: Read {
 	let destpath = &[&path, "/", &name].join("");
 	let mut dest = File::create(destpath).unwrap();
-	copy(&mut file, &mut dest);
+	copy(&mut file, &mut dest).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ use rosa::*;
 fn main() {
 	let mut img = get_file("https://torchedsammy.github.io/assets/img/profile.png");
 	download(&mut img, ".", "test.png");
+	println!("done");
 }
