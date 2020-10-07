1fbd534b22f8abe189570d1ad3119c675e59c498
Catry <catry.me@gmail.com>
2020-10-08 03:47:25 +0900

    Add download files

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,22 @@ pub fn download<F: ?Sized>(mut file: &mut F, path: &str, name: &str) where F: Re
     	_ => unreachable!(),
 	};
 }
+
+pub fn download_files(filenames: Vec<&str>, path: &str, urls: Vec<&str>) {
+	for i in urls {
+		let responses: ureq::Response = ureq::get(i).call();
+		let mut files = responses.into_reader();
+		let len = i.len();
+		let extension = &i[len - 4..];
+		
+		for b in &filenames {
+		let filename: &str = &(b.to_owned().to_owned() + extension);
+		let destpath = &[&path,"/", filename].join("");
+		let mut dest =  File::create(destpath).unwrap();
+		match copy(&mut files, &mut dest) {
+			Ok(a) => a,
+			_ => unreachable!(),
+		};
+	  }
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ use rosa::*;
 fn main() {
 	let mut img = get_file("https://torchedsammy.github.io/assets/img/profile.png");
 	download(&mut img, ".", "test.png");
+	let mut _img2 = download_files(["test1", "test2"].to_vec(), ".", ["https://torchedsammy.github.io/assets/img/background.png", "https://torchedsammy.github.io/assets/img/profile.png"].to_vec());
 }
