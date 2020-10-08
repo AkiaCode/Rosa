afb2e24e4643fd5b4c8c2b247d2ac9d26e93962a
Catry <catry.me@gmail.com>
2020-10-08 09:21:56 +0900

    patch download files

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,25 @@ pub fn get_file(url: &str) -> impl Read {
 	response.into_reader()
 }
 
+pub fn download_files(filenames: Vec<&str>, path: &str, urls: Vec<&str>) {
+	let mut b = 0;
+	for i in urls {
+		let responses: ureq::Response = ureq::get(i).call();
+		let mut files = responses.into_reader();
+		let len = i.len();
+		let extension = &i[len - 4..];
+		let file = filenames.clone()[b];
+		let filename: &str = &(file.to_string() + extension);
+		let destpath = &[&path,"/", filename].join("");
+		let mut dest =  File::create(destpath).unwrap();
+		match copy(&mut files, &mut dest) {
+			Ok(a) => a,
+			_ => unreachable!(),
+		};
+	  	b += 1;
+	}
+}
+
 pub fn download<F: ?Sized>(mut file: &mut F, path: &str, name: &str) where F: Read {
 	let destpath = &[&path, "/", &name].join("");
 	let mut dest = File::create(destpath).unwrap();
@@ -15,23 +34,4 @@ pub fn download<F: ?Sized>(mut file: &mut F, path: &str, name: &str) where F: Re
     	Ok(a) => a,
     	_ => unreachable!(),
 	};
-}
-
-pub fn download_files(filenames: Vec<&str>, path: &str, urls: Vec<&str>) {
-	for i in urls {
-		let responses: ureq::Response = ureq::get(i).call();
-		let mut files = responses.into_reader();
-		let len = i.len();
-		let extension = &i[len - 4..];
-		
-		for b in &filenames {
-		let filename: &str = &(b.to_owned().to_owned() + extension);
-		let destpath = &[&path,"/", filename].join("");
-		let mut dest =  File::create(destpath).unwrap();
-		match copy(&mut files, &mut dest) {
-			Ok(a) => a,
-			_ => unreachable!(),
-		};
-	  }
-	}
 }
